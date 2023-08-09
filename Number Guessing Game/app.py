49771fe082bfa7e3929beac1e32150667a9e37d3
kxng0109 <iluhjosh13@gmail.com>
2023-08-09 22:21:58 +0100

    My first python project

--- Number Guessing Game/app.py
+++ Number Guessing Game/app.py
@@ -4,7 +4,7 @@ random_num = random.randint(0, 10)
 used_clues = 0
 guessed_num = ""
 
-print("Can you guess the number correctly between 0 and 10?.")
+print("Can you correctly guess the number between 0 and 20 in five attempts?")
 
 while used_clues < 5 and guessed_num != random_num:
     used_clues += 1
@@ -14,6 +14,10 @@ while used_clues < 5 and guessed_num != random_num:
             print("Congrats! The number was " + str(random_num) + ". You got it in " + str(used_clues) + " tries.")
         elif guessed_num != random_num and used_clues == 5:
             print("You've used all your tries.")
+        elif guessed_num < random_num:
+            print("You're lower than the number")
+        elif guessed_num > random_num:
+            print("You're higher than the number")
         else:
             print("Wrong number. You have " + str(5 - used_clues) + " tries left. Try again.")
     except ValueError as err:
